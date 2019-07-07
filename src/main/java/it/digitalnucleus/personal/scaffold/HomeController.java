869740202b91572f4726ca9d9567f13ec8f5d9ae
Renato Perini <renato.perini@gmail.com>
2019-07-08 00:03:22 +0200

    Initial commit

---
+++ src/main/java/it/digitalnucleus/personal/scaffold/HomeController.java
@@ -0,0 +1,25 @@
+package it.digitalnucleus.personal.scaffold;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+	
+	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/log", method = RequestMethod.GET)
+    public @ResponseBody String index() {
+        logger.trace("trace logging");
+        logger.debug("debug logging");
+        logger.info("info logging");
+        logger.warn("warning logging");
+        return "Hello, World!";
+    }
+
+}
