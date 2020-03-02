00e5ab50e475c723c62284fb9e5f9b6f80ef214e
Renato Perini <renato.perini@gmail.com>
2020-03-02 23:26:40 +0100

    Minor adjustments

--- src/main/java/it/digitalnucleus/personal/scaffold/HomeController.java
+++ src/main/java/it/digitalnucleus/personal/scaffold/HomeController.java
@@ -2,8 +2,8 @@ package it.digitalnucleus.personal.scaffold;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +13,9 @@ public class HomeController {
 	
 	private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@RequestMapping(value = "/log", method = RequestMethod.GET)
-    public @ResponseBody String index() {
+	//@RequestMapping(value = "/log", method = RequestMethod.GET)
+    @GetMapping(value = "/log")
+	public @ResponseBody String index() {
         logger.trace("trace logging");
         logger.debug("debug logging");
         logger.info("info logging");
