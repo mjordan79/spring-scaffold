cc1075e5377a27fe79c26d9f4f782c282f9fe4e6
Renato Perini <renato.perini@gmail.com>
2020-03-02 23:53:28 +0100

    Migrated to JUnit 5 Jupiter and JDK 11

--- src/test/java/it/digitalnucleus/personal/scaffold/AppTest.java
+++ src/test/java/it/digitalnucleus/personal/scaffold/AppTest.java
@@ -1,34 +1,17 @@
 package it.digitalnucleus.personal.scaffold;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName
-	 *            name of the test case
-	 */
-	public AppTest(String testName) {
-		super(testName);
+public class AppTest {
+	
+	@Test
+	void rigorousTest() {
+		assertEquals(1, 1);
 	}
-
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(AppTest.class);
-	}
-
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-		assertTrue(true);
-	}
+	
 }
